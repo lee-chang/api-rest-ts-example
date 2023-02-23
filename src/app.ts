c2c3eca60f7bd0118547b7375bce2ce83fb4c16a
vchangcar <ca.y25@hotmail.com>
2023-02-23 01:16:48 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 import "dotenv/config";
-import express from "express";
+import express, {Express} from "express";
 import cors from "cors";
 import { router } from "./routes";
 import db from "./config/mongo";
+import helmet from "helmet";
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+const app: Express = express();
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
+
+// * Content Type Config
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({limit: '50mb'}));
+
+
 app.use(router);
 db().then(() =>  console.log("Conectado a la base de datos"));
+
 
 app.listen(PORT, () => {
   console.log(`Listo por el puerto -> ${PORT}`);
